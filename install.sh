44e51fecdbdd2d509556912b19ab3c97ba11a305
Saittham <matthias.herterich@gmail.com>
2024-01-06 15:44:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,6 +19,30 @@ create_symlinks() {
 
 create_symlinks
 
+#!/bin/sh
+zshrc() {
+    echo "==========================================================="
+    echo "             cloning zsh-autosuggestions                   "
+    echo "-----------------------------------------------------------"                    
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    echo "==========================================================="
+    echo "                 cloning zsh-zsh-nvm                       "
+    echo "-----------------------------------------------------------"                 
+    git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-nvm
+    echo "==========================================================="
+    echo "                  Import zshrc                             "
+    echo "-----------------------------------------------------------"
+    cat .zshrc > $HOME/.zshrc
+}
+zshrc
+
+
+
+
+
+
+
+
 echo "Initializing conda for zsh."
 conda init zsh
 
